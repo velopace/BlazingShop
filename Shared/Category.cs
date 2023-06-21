540592924820b9ecb522c34dae9343ad53842d52
velopace <velopace@outlook.com>
2023-06-21 15:13:56 -0500

    Init shared models

---
+++ Shared/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace BlazingShop.Shared
+{
+	public class Category
+	{
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Url { get; set; }
+        public string? Icon { get; set; }
+        public List<Product>? Products { get; set; }
+    }
+}
+
