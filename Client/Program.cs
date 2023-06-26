642f63065f87997e8fd94f87916af7ae33a619f1
velopace <velopace@outlook.com>
2023-06-26 16:17:40 -0400

    filter product with category

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazingShop.Client;
 using BlazingShop.Client.Services.ProductService;
+using BlazingShop.Client.Services.CategoryService;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,6 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 await builder.Build().RunAsync();
 
---
+++ Client/Services/CategoryService/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using BlazingShop.Shared;
+
+namespace BlazingShop.Client.Services.CategoryService
+{
+	public interface ICategoryService
+	{
+		List<Category> Categories { get; set; }
+		public void LoadCategories();
+	}
+}
+
