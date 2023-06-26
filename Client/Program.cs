6bc4a2109177f77bd10a1f8bee7e1b2439bd9a9c
velopace <velopace@outlook.com>
2023-06-26 15:30:33 -0400

    add product service and detail page

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazingShop.Client;
+using BlazingShop.Client.Services.ProductService;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IProductService, ProductService>();
 
 await builder.Build().RunAsync();
 
---
+++ Client/Services/ProductService/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using BlazingShop.Shared;
+
+namespace BlazingShop.Client.Services.ProductService
+{
+	public interface IProductService
+	{
+		List<Product> Products { get; set; }
+		void LoadProducts();
+	}
+}
+
