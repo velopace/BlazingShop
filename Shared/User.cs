5a78b872b4c726d14eed9557f156e780087db066
velopace <velopace@outlook.com>
2023-07-08 14:45:32 -0400

    login page

---
+++ Shared/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace BlazingShop.Shared
+{
+	public class User
+	{
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
+
