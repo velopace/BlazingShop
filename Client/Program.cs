948b54a188f54228a5646a20fea7e9c448a116ba
velopace <velopace@outlook.com>
2023-07-08 04:09:56 -0400

    setup localstorage and toast

--- Client/Program.cs
+++ Client/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazingShop.Client;
 using BlazingShop.Client.Services.ProductService;
 using BlazingShop.Client.Services.CategoryService;
+using Blazored.LocalStorage;
+using Blazored.Toast;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,6 +13,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddBlazoredToast();
 
 await builder.Build().RunAsync();
 
